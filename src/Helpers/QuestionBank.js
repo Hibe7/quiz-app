9d49eb46fb79b8649b6e6c3b3a8eaf0ca090bee8
Hibe7 <74568584+Hibe7@users.noreply.github.com>
2021-09-29 01:48:15 +0200

    commit

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,27 @@
+export const Questions = [
+    {
+        prompt: "What is 1+3?",
+        optionA: "5",
+        optionB: "2",
+        optionC: "6",
+        optionD: "4",
+        answer: "D",
+    },
+    {
+        prompt: "Which is a Streaming Platform?",
+        optionA: "Twitter",
+        optionB: "Twitch",
+        optionC: "Google",
+        optionD: "Netflix",
+        answer: "B",
+    },
+    {
+        prompt: "What is 5*3?",
+        optionA: "15",
+        optionB: "22",
+        optionC: "14",
+        optionD: "16",
+        answer: "A",
+    },
+
+]
